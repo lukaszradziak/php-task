6448071bb3b303459fea68d9d20755dccae051dc
Łukasz Radziak <lukasilestam@gmail.com>
2022-04-26 22:11:40 +0200

    Add vars and methods to Product

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -2,6 +2,52 @@
 
 namespace Recruitment\Entity;
 
+use InvalidArgumentException;
+use Recruitment\Entity\Exception\InvalidUnitPriceException;
+
 class Product
 {
+    private $id;
+
+    private $unitPrice;
+
+    private $minimumQuantity;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getUnitPrice(): int
+    {
+        return $this->unitPrice;
+    }
+
+    public function setUnitPrice(int $unitPrice): void
+    {
+        if ($unitPrice <= 0) {
+            throw new InvalidUnitPriceException();
+        }
+
+        $this->unitPrice = $unitPrice;
+    }
+
+    public function getMinimumQuantity(): int
+    {
+        return $this->minimumQuantity;
+    }
+
+    public function setMinimumQuantity($minimumQuantity): void
+    {
+        if ($minimumQuantity <= 0) {
+            throw new InvalidArgumentException();
+        }
+
+        $this->minimumQuantity = $minimumQuantity;
+    }
 }
