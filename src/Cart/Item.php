fe8973f92ec848e2c88dff5b4eb7479b4ae6b0b4
Łukasz Radziak <lukasilestam@gmail.com>
2022-04-26 22:21:32 +0200

    Add vars and methods to Item

--- src/Cart/Item.php
+++ src/Cart/Item.php
@@ -2,6 +2,46 @@
 
 namespace Recruitment\Cart;
 
+use Recruitment\Cart\Exception\QuantityTooLowException;
+use Recruitment\Entity\Product;
+
 class Item
 {
+    private $product;
+
+    private $quantity;
+
+    public function __construct(Product $product, int $quantity)
+    {
+        if ($product->getMinimumQuantity() > $quantity) {
+            throw new \InvalidArgumentException();
+        }
+
+        $this->product = $product;
+        $this->quantity = $quantity;
+    }
+
+    public function getProduct(): Product
+    {
+        return $this->product;
+    }
+
+    public function getTotalPrice(): int
+    {
+        return $this->quantity * $this->product->getUnitPrice();
+    }
+
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): void
+    {
+        if ($this->product->getMinimumQuantity() > $quantity) {
+            throw new QuantityTooLowException();
+        }
+
+        $this->quantity = $quantity;
+    }
 }
